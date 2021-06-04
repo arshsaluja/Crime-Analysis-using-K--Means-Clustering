336c6177991a018574ac7d596039a50a740fad29
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-06-04 21:47:11 +0530

    Create After applying K-means.py

---
+++ Mid Sem/After applying K-means.py
@@ -0,0 +1,21 @@
+import seaborn as sns
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import sklearn.cluster as cluster
+df=pd.read_csv('../Downloads/20_Victims_of_rape.csv')
+df
+df.isna().sum()
+df.describe
+sns.pairplot(df[['Year','Rape_Cases_Reported','Victims_Above_50_Yrs','Victims_Between_14-18_Yrs','Victims_of_Rape_Total','Victims_Upto_10_Yrs']])
+kmeans = cluster.KMeans(n_clusters=5 ,init="k-means++")
+kmeans=kmeans.fit(df[['Rape_Cases_Reported','Victims_Upto_10_Yrs']])
+kmeans.cluster_centers_
+df['Clusters']=kmeans.labels_
+df.head()
+df['Clusters'].value_counts()
+
+sns.scatterplot(x="Rape_Cases_Reported", y="Victims_Upto_10_Yrs", hue='Clusters', data=df)
+sns.scatterplot(x="Rape_Cases_Reported", y="Victims_Between_10-14_Yrs", hue='Clusters', data=df)
+sns.scatterplot(x="Rape_Cases_Reported", y="Victims_Between_14-18_Yrs", hue='Clusters', data=df)
+sns.scatterplot(x="Rape_Cases_Reported", y="Victims_Between_18-30_Yrs", hue='Clusters', data=df)
+sns.scatterplot(x="Rape_Cases_Reported", y="Victims_Between_30-50_Yrs", hue='Clusters', data=df)
+sns.scatterplot(x="Rape_Cases_Reported", y="Victims_Above_50_Yrs", hue='Clusters', data=df)
